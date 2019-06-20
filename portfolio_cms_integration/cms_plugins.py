e462d6c3503ffff0a56290051a29b4d47dba8dd7
Ramesh Pradhan <ramesrest@gmail.com>
2019-06-20 16:37:01 +0545

    Porject and Portfolio plugins added

--- portfolio_cms_integration/cms_plugins.py
+++ portfolio_cms_integration/cms_plugins.py
@@ -1,15 +1,27 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from .models import PortfolioPluginModel
+from .models import PortfolioPluginModel, ProjectPluginModel
 from django.utils.translation import ugettext as _
 
 
 @plugin_pool.register_plugin  # register the plugin
-class PollPluginPublisher(CMSPluginBase):
+class PortfolioPluginPublisher(CMSPluginBase):
     model = PortfolioPluginModel  # model where plugin data are saved
     module = _("Portfolio")
     name = _("Portfolio Plugin")  # name of the plugin in the interface
     render_template = "portfolio_cms_integration/portfolio_plugin.html"
+
+    def render(self, context, instance, placeholder):
+        context.update({'instance': instance})
+        return context
+
+
+@plugin_pool.register_plugin  # register the plugin
+class ProjectPluginPublisher(CMSPluginBase):
+    model = ProjectPluginModel  # model where plugin data are saved
+    module = _("Project")
+    name = _("Project Plugin")  # name of the plugin in the interface
+    render_template = "portfolio_cms_integration/project_plugin.html"
 
     def render(self, context, instance, placeholder):
         context.update({'instance': instance})
--- portfolio_cms_integration/models.py
+++ portfolio_cms_integration/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from cms.models import CMSPlugin
-from portfolio_app.models import Portfolio
+from portfolio_app.models import Portfolio, Project
 
 
 class PortfolioPluginModel(CMSPlugin):
 	portfolio = models.ManyToManyField(Portfolio)
+	# title = models.CharField(max_length = 150)
 
-	def __str__(self):
-		return self.portfolio.author
+
+	# def __str__(self):
+	# 	return self.title
+
+class ProjectPluginModel(CMSPlugin):
+	author = models.ManyToManyField(Project)
